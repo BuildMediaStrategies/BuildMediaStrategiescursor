10338e69a0e80df6dc4c1af54258db77da8c2751
BuildMediaStrategies <buildmediastrategies@outlook.com>
2025-11-13 08:51:36 +0000

    Updated Header.tsx

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -13,7 +13,7 @@ export default function Header() {
           <img
             src="/assets/bmsnewlogo sky.png"
             alt="Build Media Strategies"
-            className="h-8 sm:h-10 md:h-12 w-auto object-contain scale-[3.25] sm:scale-[3.75] origin-left translate-y-[6px]"
+            className="h-8 sm:h-10 md:h-12 w-auto object-contain scale-[3.25] sm:scale-[3.75] origin-left translate-y-[12px]"
             width="320"
             height="88"
             loading="eager"
